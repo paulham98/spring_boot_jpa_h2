26d6a26390350f93c533513b2a946f395fecc7db
hamjungho <pauljungho.ham@gmail.com>
2019-01-02 18:02:52 +0900

    fourth commit

--- src/main/java/com/transaction/h2/H2Application.java
+++ src/main/java/com/transaction/h2/H2Application.java
@@ -2,13 +2,13 @@ package com.transaction.h2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class H2Application {
+public class H2Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(H2Application.class, args);
     }
 
 }
-
--- src/main/java/com/transaction/h2/services/HelloService.java
+++ src/main/java/com/transaction/h2/services/HelloService.java
@@ -1,11 +1,15 @@
 package com.transaction.h2.services;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HelloService {
 
+    @Value("hello.world")
+    private String world;
+
     public String getHello() {
-        return "hello";
+        return "profile hello.world:"+world;
     }
 }
