bd4a2092945afd4d54548d64da5b040761914adf
hamjungho <pauljungho.ham@gmail.com>
2019-01-14 11:34:47 +0900

    nineth commit_HelloServiceTest 오류

--- src/main/java/com/transaction/h2/repositories/UserRepository.java
+++ src/main/java/com/transaction/h2/repositories/UserRepository.java
@@ -3,6 +3,6 @@ package com.transaction.h2.repositories;
 import com.transaction.h2.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long> {
 }
 
--- src/main/java/com/transaction/h2/services/HelloService.java
+++ src/main/java/com/transaction/h2/services/HelloService.java
@@ -1,15 +1,31 @@
 package com.transaction.h2.services;
 
+import com.transaction.h2.domain.User;
+import com.transaction.h2.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class HelloService {
+
+
+    private UserRepository userRepository;
+
+    public HelloService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Value("hello.world")
     private String world;
 
     public String getHello() {
         return "profile hello.world:"+world;
+    }
+
+    public List<User> getUsers() {
+        List<User> user = userRepository.findAll();
+        return user;
     }
 }
