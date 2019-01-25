a30c57edf782db4e8f7b6bd18dde9e055a261c0a
hamjungho <pauljungho.ham@gmail.com>
2019-01-25 16:07:34 +0900

    21th.commit:Dockerfile_route

--- src/main/java/com/transaction/h2/H2Application.java
+++ src/main/java/com/transaction/h2/H2Application.java
@@ -4,13 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableCaching
 public class H2Application extends SpringBootServletInitializer {
 
+
+
+    @RequestMapping
+    public String home(){
+        return "Hello Docker World";
+
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(H2Application.class, args);
     }
+
+
 
 }
